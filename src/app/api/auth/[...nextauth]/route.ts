b6978b478026fe435f4ce7b463de17887104302e
dmytrohrechanyi <zarubavoobshe2120@gmail.com>
2024-08-26 10:30:54 +0100

    update microsoft auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,17 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
+import MicrosoftProvider from "next-auth/providers/azure-ad";
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: String(process.env.GOOGLE_CLIENT_ID),
       clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
-    })   
+    }),  
+    MicrosoftProvider({
+      clientId: String(process.env.MICROSOFT_CLIENT_ID),
+      clientSecret: String(process.env.MICROSOFT_CLIENT_SECRET),
+      tenantId: process.env.MICROSOFT_TENANT_ID, // Optional
+    }),   
   ],
 };
 
